88b80b6ddfa8f31c2887d8addd47e6a10bbbc073
Arjen Wiersma <arjen@wiersma.org>
2021-10-04 20:49:27 +0200

    Add some samples, implement hostentry methods

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,310 @@
+use clap::{App, Arg};
+use color_eyre::eyre::eyre;
+pub use color_eyre::eyre::Result;
+use std::fmt;
 use std::fs::File;
-use std::io::{self, BufRead};
+use std::io::{self, BufRead, Error, ErrorKind};
 use std::net::IpAddr;
 use std::path::Path;
 
-fn main() {
-    println!(
-        "{:#?}",
-        parse_line("10.10.10.111 forge.htb admin.forge.htb")
-    );
-    // File hosts must exist in current path before this produces output
-    if let Ok(lines) = read_lines("./hosts") {
-        // Consumes the iterator, returns an (Optional) String
-        for line in lines {
-            match line {
-                Ok(ip) => {
-                    println!("{}", ip);
+use regex::Regex;
+
+fn main() -> Result<()> {
+    color_eyre::install()?;
+
+    let matches = App::new("dotfiles")
+        .version("0.1")
+        .author("Arjen Wiersma <arjen@wiersma.org")
+        .about("Version control all your dotfiles")
+        .setting(clap::AppSettings::ColoredHelp)
+        .setting(clap::AppSettings::ArgRequiredElseHelp)
+        .arg(
+            Arg::new("file")
+                .long("file")
+                .required(false)
+                .takes_value(true)
+                .about("Instead of /etc/hosts, use this file (testing)"),
+        )
+        .subcommand(
+            App::new("verify")
+                .about("Verify the integrity of the hosts file")
+                .version("0.1"),
+        )
+        .subcommand(
+            App::new("show")
+                .about("List your current hostfile")
+                .version("0.1"),
+        )
+        .subcommand(
+            App::new("add")
+                .about("Add a host to your hostfile")
+                .version("0.1")
+                .arg(
+                    Arg::new("hostname")
+                        .index(1)
+                        .required(true)
+                        .about("Hostname to add to the hostfile"),
+                )
+                .arg(
+                    Arg::new("ip")
+                        .index(2)
+                        .required(false)
+                        .about("Optional: ip address of the hostname"),
+                ),
+        )
+        .get_matches();
+
+    let res = match matches.subcommand_name() {
+        Some("verify") => verify(get_filename(&matches)),
+        Some("show") => show(get_filename(&matches)),
+        Some("add") => {
+            let mymatches = matches
+                .subcommand_matches("add")
+                .expect("Cannot be a subcommand and not be a subcommand");
+
+            let ip = if mymatches.is_present("ip") {
+                mymatches.value_of("ip")
+            } else {
+                None
+            };
+            add(get_filename(&matches), mymatches.value_of("hostname"), ip)
+        }
+        Some(x) => {
+            println!("Unimplemented command {} called", x);
+            unimplemented!()
+        }
+        _ => {
+            println!("No subcommand given");
+            unimplemented!()
+        }
+    };
+    res
+}
+
+fn get_filename(matches: &clap::ArgMatches) -> &str {
+    match matches.value_of("file") {
+        Some(x) => x,
+        _ => "/etc/hosts",
+    }
+}
+
+fn get_hostentries(filename: &str) -> Result<Vec<Option<HostEntry>>> {
+    match parse_file(filename) {
+        Ok(x) => Ok(x),
+        Err(x) => Err(eyre!("Failed to parse file {}", x)),
+    }
+}
+
+fn add(filename: &str, hostname: Option<&str>, ip: Option<&str>) -> Result<(), color_eyre::Report> {
+    let ip_address: Option<IpAddr> = match ip {
+        Some(x) => Some(x.parse()?),
+        _ => None,
+    };
+
+    let hosts = get_hostentries(filename)?;
+
+    println!("Adding {:#?} {:#?}", hostname, ip_address);
+
+    // if IP address is given, find a matching hostentry to add a alias
+    //    no ip? add new entry
+    // if only a name is given, find a HostEntry already serving a tld
+    //    if none are found, err
+    if ip_address.is_some() {
+        println!("Adding ip: {:#?}", ip_address);
+
+        for item in hosts {
+            if item.is_some() {
+                let mut i = item.unwrap();
+                if i.ip == ip_address {
+                    println!("Adding the same ip: {:#?}", ip_address);
+                    if !i.has_name(hostname.unwrap()) {
+                        i.add_alias(hostname.unwrap());
+                    }
                 }
-                _ => (),
             }
         }
+    }
+
+    Ok(())
+}
+
+fn show(filename: &str) -> Result<(), color_eyre::Report> {
+    let hosts = get_hostentries(filename)?;
+
+    for item in hosts {
+        println!("{}", item.unwrap_or_else(HostEntry::empty))
+    }
+    Ok(())
+}
+
+fn verify(filename: &str) -> Result<(), color_eyre::Report> {
+    match parse_file(filename) {
+        Ok(x) => {
+            println!("Hostsfile is readable and contains {} entries", x.len());
+            Ok(())
+        }
+        Err(x) => Err(eyre!("Failed to parse file {}", x)),
     }
 }
 
 #[derive(Debug)]
 pub struct HostEntry {
-    ip: IpAddr,
-    name: String,
-    aliasses: Vec<String>,
+    ip: Option<IpAddr>,
+    name: Option<String>,
+    aliasses: Option<Vec<String>>,
+    comment: Option<String>,
+}
+
+impl HostEntry {
+    fn empty() -> HostEntry {
+        HostEntry {
+            ip: None,
+            name: None,
+            aliasses: None,
+            comment: None,
+        }
+    }
+
+    /// Checks if the `name` of `HostEntry` can result the passed `hostname`.
+    ///
+    /// If `name` is `host.tld` and `hostname` is a subdomain, return true.
+    /// If `name` is a subdomain `sub.host.tld` of `hostname`, return false.
+    ///
+    /// A subdomain is more specific, to rule out overlap, do not change it.
+    /// TODO: allow reassigning of `name`
+    fn can_resolve_host(&self, hostname: &str) -> bool {
+        hostname.ends_with(self.name.as_ref().unwrap().as_str())
+    }
+
+    fn has_ip(&self, ip: &IpAddr) -> bool {
+        self.ip.as_ref().unwrap() == ip
+    }
+
+    fn has_name(&self, hostname: &str) -> bool {
+        match &self.name {
+            Some(x) => {
+                if x.eq(hostname) {
+                    return true;
+                }
+            }
+            _ => {}
+        }
+        match &self.aliasses {
+            Some(y) => {
+                for z in y {
+                    println!("Checking {} against {}", z, hostname);
+                    if z == hostname {
+                        return true;
+                    }
+                }
+            }
+            _ => {
+                return false;
+            }
+        }
+        false
+    }
+
+    pub(crate) fn add_alias(&mut self, hostname: &str) {
+        // if self.aliasses.is_some() {
+        //     let s = self.aliasses.as_ref().unwrap();
+        //     s.push(String::from(hostname));
+        // }
+        todo!()
+    }
+}
+
+impl PartialEq for HostEntry {
+    fn eq(&self, other: &Self) -> bool {
+        self.ip == other.ip
+            && self.name == other.name
+            && self.aliasses == other.aliasses
+            && self.comment == other.comment
+    }
+}
+
+// impl PartialOrd for HostEntry {
+//     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+//         todo!()
+//     }
+// }
+
+impl fmt::Display for HostEntry {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        if self.ip == None && self.comment != None {
+            write!(f, "# {}", self.comment.as_ref().unwrap())
+        } else if self.ip != None {
+            write!(
+                f,
+                "{}\t{}\t{}",
+                self.ip.unwrap(),
+                self.name.as_ref().unwrap(),
+                self.aliasses.as_ref().unwrap_or(&vec![]).join(" ")
+            )
+        } else {
+            write!(f, "")
+        }
+    }
+}
+
+fn parse_file(filename: &str) -> Result<Vec<Option<HostEntry>>, Box<dyn std::error::Error>> {
+    match read_lines(filename) {
+        Ok(lines) => Ok(lines
+            .map(
+                |l| match parse_line(l.unwrap_or_else(|_e| "".to_string()).as_str()) {
+                    Ok(s) => Some(s),
+                    _ => None,
+                },
+            )
+            .collect()),
+        Err(e) => Err(Box::new(Error::new(
+            ErrorKind::Other,
+            format!("Failed to parse file {}", e),
+        ))),
+    }
 }
 
 fn parse_line(input: &str) -> Result<HostEntry, Box<dyn std::error::Error>> {
-    Ok(HostEntry {
-        ip: "10.10.10.111".parse()?,
-        name: String::from("forge.htb"),
-        aliasses: vec![String::from("admin.forge.htb")],
-    })
+    let comment = Regex::new(r"^#(?P<c>.+)\s*$").unwrap();
+    let entry = Regex::new(r"^(?P<ip>.+?)\s+(?P<name>.+?)(\s+(?P<aliasses>.+))?$").unwrap();
+    if comment.is_match(input) {
+        Ok(comment
+            .captures(input)
+            .map(|cap| HostEntry {
+                ip: None,
+                name: None,
+                aliasses: None,
+                comment: cap.name("c").map(|t| String::from(t.as_str())),
+            })
+            .unwrap())
+    } else if entry.is_match(input) {
+        let caps = entry.captures(input).unwrap();
+        let ip_str = caps.name("ip").map(|t| t.as_str()).unwrap();
+
+        let ip: Option<IpAddr> = match ip_str.parse() {
+            Ok(x) => Some(x),
+            _ => None,
+        };
+
+        let name = caps.name("name").map(|t| String::from(t.as_str()));
+        let alias = caps.name("aliasses").map(|t| String::from(t.as_str()));
+        let alias_vec: Option<Vec<String>> = alias.map(|x| {
+            x.split_whitespace()
+                .map(String::from)
+                .collect::<Vec<String>>()
+        });
+        Ok(HostEntry {
+            ip,
+            name,
+            aliasses: alias_vec,
+            comment: None,
+        })
+    } else {
+        Err(Box::new(Error::new(
+            ErrorKind::Other,
+            "Failed to read host string",
+        )))
+    }
 }
 
 // The output is wrapped in a Result to allow matching on errors
@@ -61,5 +331,34 @@ mod test {
 
         assert_eq!("127.0.0.1".parse(), Ok(localhost_v4));
         assert_eq!("::1".parse(), Ok(localhost_v6));
+    }
+
+    #[test]
+    fn test_hostentry_has_name() {
+        let he = HostEntry {
+            ip: None,
+            name: Some(String::from("arjen.wiersma.nl")),
+            aliasses: None,
+            comment: None,
+        };
+
+        assert!(he.has_name("arjen.wiersma.nl"));
+        assert!(!he.has_name("arjen2.wiersma.nl"));
+        assert!(!he.has_name("wiersma.nl"));
+
+        let ahe = HostEntry {
+            ip: None,
+            name: Some(String::from("jelle.wiersma.nl")),
+            aliasses: Some(vec![
+                String::from("arjen.wiersma.nl"),
+                String::from("rebecca.wiersma.nl"),
+            ]),
+            comment: None,
+        };
+        assert!(ahe.has_name("arjen.wiersma.nl"));
+        assert!(ahe.has_name("rebecca.wiersma.nl"));
+        assert!(!ahe.has_name("arjen2.wiersma.nl"));
+        assert!(!ahe.has_name("wiersma.nl"));
+        assert!(ahe.has_name("jelle.wiersma.nl"));
     }
 }
